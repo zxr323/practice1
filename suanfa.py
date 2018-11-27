953a44a6736b0ea4575c943060cef126e4ca54a0
Zhengxiaorong <892119356@qq.com>
2018-11-27 23:49:33 +0800

    quick_sort,bubble_sort

---
+++ suanfa.py
@@ -0,0 +1,31 @@
+#徒手写冒泡
+def bubble_sort(lis):
+    for i in range(1,len(lis)):
+        for j in range(0,len(lis)-i):
+            if lis[j]>lis[j+1]:
+                lis[j],lis[j+1] = lis[j+1],lis[j]
+    return lis
+lis = [1,2,3,5,3,4,7,11,9]
+print(bubble_sort(lis))
+
+
+#徒手写快排
+lis = [5,3,2,6,8,9,7,1]
+def quick_sort(lis,left,right):
+    if left<right:
+        mid = partition(lis,left,right)
+        quick_sort(lis,left,mid-1)
+        quick_sort(lis,mid+1,right)
+    return lis
+def partition(lis,left,right):
+    while left<right:
+        tmp = lis[left]
+        while left<right and lis[right]>tmp:
+            right -= 1
+        lis[left] = lis[right]
+        while left<right and lis[left]<tmp:
+            left += 1
+        lis[right] = lis[left]
+        lis[left] = tmp
+    return left
+print(quick_sort(lis,0,len(lis)-1))   
